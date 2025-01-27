a9a492faf499aeb5fcede45d504993629a30a46c
Kin Cunico <124842064+kin-cunico@users.noreply.github.com>
2025-01-27 16:29:53 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig ({
     base: '/kanji_project',
     build: {
-        assetsInclude: ['**/*.json'],
-        outDir: "dist"
+        assetsInclude: ['**/*.json']
     }
 })
